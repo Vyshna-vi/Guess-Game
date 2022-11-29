50a2e26c521cd5644d9de33dfd778ed473680110
Vyshnavi <vyshnaviponnuzz0001@gmail.com>
2022-11-29 09:13:58 +0530

    completed guessgaming

--- gg.js
+++ gg.js
@@ -1,4 +1,5 @@
 let answer;
+let checkans;
 let start = document.getElementById("btn");
 console.log(start);
 start.addEventListener("click", function (e) {
@@ -8,4 +9,32 @@ start.addEventListener("click", function (e) {
       console.log(data[0]);
       answer = data[0];
     });
+});
+let check = document.getElementById("btn1");
+// console.log(check);
+check.addEventListener("click", function (e) {
+  let input = document.getElementById("inpt");
+  // console.log(input);
+  checkans = input.value;
+  console.log(checkans);
+  if (checkans == answer) {
+    alert("Success");
+  } else {
+    alert("Wrong");
+  }
+});
+let hint = document.getElementById("btn2");
+// console.log(hint);
+let hintcount = 0;
+hint.addEventListener("click", function (e) {
+  if (hintcount == 0) {
+    let p1 = document.getElementById("para2");
+    // console.log(p1);
+    p1.innerHTML = "> Word has " + answer.length + " letters";
+    hintcount = 1;
+    // console.log(hintcount);
+  } else if (hintcount == 1) {
+    let p2 = document.getElementById("para3");
+    p2.innerHTML = "> First letter is " + answer[0];
+  }
 });
